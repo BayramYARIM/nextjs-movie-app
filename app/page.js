8228fe4ea42649548ae8e06060ffa0384b21cb0d
BAYRAM YARIM <byyarim@hotmail.com>
2024-09-27 18:30:49 +0300

    Categories have been added to the homepage. Page modified

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 import Movies from "@/data-mocks/movies.json";
+import DataCategories from "@/data-mocks/movie-categories.json";
 import { BackgorundMoviePoster } from "@/components/background-movie";
+import { Categories } from "@/components/categories";
 
 export default function Home() {
   return (
     <div>
       <BackgorundMoviePoster movie={Movies.results[12]} />
+      <Categories categories={DataCategories.genres} />
     </div>
   );
 }
