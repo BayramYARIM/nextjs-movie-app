17cc71cde3b741f5b82c176fda261c15775bb782
BAYRAM YARIM <byyarim@hotmail.com>
2024-10-01 11:12:01 +0300

    Modified main page - Added MovieCard and MovieStateCard

--- app/page.js
+++ app/page.js
@@ -4,12 +4,27 @@ import Movies from "@/data-mocks/movies.json";
 import DataCategories from "@/data-mocks/movie-categories.json";
 import { BackgorundMoviePoster } from "@/components/background-movie";
 import { Categories } from "@/components/categories";
+import { MoviesCard } from "@/components/movie-card";
+import { MovieStateCard } from "@/components/movie-statecard";
 
 export default function Home() {
   return (
     <div>
       <BackgorundMoviePoster movie={Movies.results[12]} />
       <Categories categories={DataCategories.genres} />
+      <MovieStateCard 
+        title="Released Films"
+        movies={Movies.results.slice(8, 18)} />
+      <MoviesCard 
+        title="Recommended" 
+        movies={Movies.results.slice(0, 12)} />
+      <MoviesCard
+        title="Upcoming Films"
+        movies={Movies.results.slice(14, 20)} />
+      <MoviesCard 
+        title="Top 10 Films" 
+        movies={Movies.results.slice(10, 16)} />
+
     </div>
   );
 }
